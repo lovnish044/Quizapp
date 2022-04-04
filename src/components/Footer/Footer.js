94dfcff96ed806eee112cdcdb8226ace59cae839
lovnish044 <lovnishgarg044@gmail.com>
2022-04-04 19:32:25 +0530

    first

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made by Lovnish Garg
+     
+    </div>
+  );
+};
+
+export default Footer;
